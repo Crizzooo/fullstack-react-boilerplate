1faae57bdad01680543c9f658b95a9abb2ea06d4
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2017-05-17 19:12:52 -0400

    feat(starting to remove MDB and implement material-ui)

--- client/src/components/EmailSplash.jsx
+++ client/src/components/EmailSplash.jsx
@@ -20,45 +20,84 @@ class EmailSplash extends Component {
   }
 
   render() {
-    return (
-      <div className="splashBackground">
-        <div className="emailSignUp">
-          <div className="signupHeader">
-            <h1>cashgrounds</h1>
-            <h6>compete. survive. win cash!</h6>
-          </div>
-          <form action="//play.us15.list-manage.com/subscribe/post?u=d806631c2c2b68285d31de691&amp;id=c29285afce" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
-            <div id="mergeTable" className="mergeTable row">
-
-              <div className="mergeRow dojoDndItem mergeRow-email emailFormInputperformance col-sm-6" id="mergeRow-0">
-                <label htmlFor="MERGE0">Email Address <span className="req asterisk">*</span></label>
-                <div className="field-group input-field">
-                  <input type="email" autoCapitalize="off" autoCorrect="off" name="MERGE0" id="MERGE0" size="25"></input>
-                </div>
-              </div>
-
-              <div className="mergeRow dojoDndItem mergeRow-text emailFormInputperformance col-sm-6" id="mergeRow-1">
-                <label htmlFor="MERGE1">Gamer Name</label>
-                <div className="field-group">
-                  <input type="text" name="MERGE1" id="MERGE1" size="25"></input>
-                </div>
-              </div>
-            </div>
-
-            <input type="hidden" name="u" value="d806631c2c2b68285d31de691"></input>
-            <input type="hidden" name="id" value="c29285afce"></input>
-
-
-            <div className="submit_container clear">
-              <input type="submit" className="button" name="submit" value="Subscribe to list"></input>
-            </div>
-            <input type="hidden" name="ht" value="b22f7a9abaa70e0da9ad920a44daea817c852b2f:MTQ5NTA0MDUzMC4yNTM3"></input>
-            <input type="hidden" name="mc_signupsource" value="hosted"></input>
-        </form>
+    return (<div className="row">
+    <form className="col-md-12">
+      <div className="row">
+        <div className="input-field col-md-6">
+          <input placeholder="Placeholder" id="first_name" type="text" className="validate"></input>
+          <label htmlFor="first_name">First Name</label>
+        </div>
+        <div className="input-field col-md-6">
+          <input id="last_name" type="text" className="validate"></input>
+          <label htmlFor="last_name">Last Name</label>
+        </div>
       </div>
-    </div>
-    );
+      <div className="row">
+        <div className="input-field col-md-12">
+          <input disabled value="I am not editable" id="disabled" type="text" className="validate"></input>
+          <label htmlFor="disabled">Disabled</label>
+        </div>
+      </div>
+      <div className="row">
+        <div className="input-field col-md-12">
+          <input id="password" type="password" className="validate"></input>
+          <label htmlFor="password">Password</label>
+        </div>
+      </div>
+      <div className="row">
+        <div className="input-field col-md-12">
+          <input id="email" type="email" className="validate"></input>
+          <label htmlFor="email">Email</label>
+        </div>
+      </div>
+    </form>
+  </div>);
   }
+  // render() {
+  //   return (
+  //     <div className="splashBackground">
+  //       <div className="emailSignUp">
+  //         <div className="signupHeader">
+  //           <h1>cashgrounds</h1>
+  //           <h6>compete. survive. win cash!</h6>
+  //         </div>
+  //         <form action="//play.us15.list-manage.com/subscribe/post?u=d806631c2c2b68285d31de691&amp;id=c29285afce" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
+  //           <div id="mergeTable" className="mergeTable row">
+  //
+  //             <div className="mergeRow dojoDndItem mergeRow-email emailFormInputperformance col-sm-6" id="mergeRow-0">
+  //               <div className="input-field">
+  //                 <input type="email" autoCapitalize="off" autoCorrect="off" name="MERGE0" id="MERGE0" size="25" type="email" className="validate"></input>
+  //                 <label htmlFor="MERGE0">Email Address <span className="req asterisk">*</span></label>
+  //               </div>
+  //             </div>
+  //
+  //             <div className="mergeRow dojoDndItem mergeRow-text emailFormInputperformance col-sm-6" id="mergeRow-1">
+  //               <label htmlFor="MERGE1">Gamer Name</label>
+  //               <div className="field-group">
+  //                 <input type="text" name="MERGE1" id="MERGE1" size="25"></input>
+  //               </div>
+  //             </div>
+  //
+  //             <div className="input-field col-md-6">
+  //        <input placeholder="Placeholder" id="first_name" type="text" className="validate"></input>
+  //        <label htmlFor="first_name">First Name</label>
+  //      </div>
+  //           </div>
+  //
+  //           <input type="hidden" name="u" value="d806631c2c2b68285d31de691"></input>
+  //           <input type="hidden" name="id" value="c29285afce"></input>
+  //
+  //
+  //           <div className="submit_container clear">
+  //             <input type="submit" className="button" name="submit" value="Subscribe to list"></input>
+  //           </div>
+  //           <input type="hidden" name="ht" value="b22f7a9abaa70e0da9ad920a44daea817c852b2f:MTQ5NTA0MDUzMC4yNTM3"></input>
+  //           <input type="hidden" name="mc_signupsource" value="hosted"></input>
+  //       </form>
+  //     </div>
+  //   </div>
+  //   );
+//  }
 }
 
 // EXAMPLE
