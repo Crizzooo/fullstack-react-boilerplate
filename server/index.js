6f6072c702d60143719e232d965bff472b0cd2f8
Mike Peritz <mike.peritz@gmail.com>
2017-06-13 15:18:57 -0400

    added PORT env variable, and handled google oauth secrets

--- server/index.js
+++ server/index.js
@@ -3,10 +3,14 @@ const express = require('express');
 const volleyball = require('volleyball');
 const bodyParser = require('body-parser');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  require('../secrets');
+}
 
 //logging middleware
 app.use(volleyball);
