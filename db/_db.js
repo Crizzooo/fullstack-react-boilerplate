16166956cda8a7646689b77932c555f8cd954809
Crizzooo <christopher.m.rizzo@vanderbilt.edu>
2017-09-17 21:14:50 -0400

    working oauth google and local buttons and db

--- db/_db.js
+++ db/_db.js
@@ -2,6 +2,9 @@ const Sequelize = require('sequelize');
 
 const databaseName = process.env.DATABASE_NAME || require('../package.json').name;
 
-const db = new Sequelize(process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`, { logging: false });
+const db = new Sequelize(process.env.DATABASE_URL || `postgres://localhost:5432/cryptoapp`, {
+    logging: false,
+    dialect: 'postgres'
+});
 
 module.exports = db;
--- server/index.js
+++ server/index.js
@@ -12,12 +12,12 @@ if (process.env.NODE_ENV === 'development') {
   require('../secrets');
 }
 
-// const db = require('../db');
-// const User = db.model('users');
+const db = require('../db');
+const User = db.model('users');
 //
 // // store session on the db so that if a server restart happens, logged in users won't be affected
-// const dbStore = new SequelizeStore({ db });
-// dbStore.sync();
+const dbStore = new SequelizeStore({ db });
+dbStore.sync();
 
 const PORT = process.env.PORT || 3000;
 
@@ -33,62 +33,62 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // session middleware
-// app.use(session({
-//   secret: process.env.LOCAL_SECRET || 'SUPER SECRET OMG!',
-//   store: dbStore,
-//   resave: false,
-//   saveUninitialized: false
-// }));
+app.use(session({
+  secret: process.env.LOCAL_SECRET || 'SUPER SECRET OMG!',
+  store: dbStore,
+  resave: false,
+  saveUninitialized: false
+}));
 
 // initialize passport
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 //
-// passport.serializeUser((user, done) => {
-//   try {
-//     done(null, user.id);
-//   } catch (err) {
-//     done(err);
-//   }
-// });
+passport.serializeUser((user, done) => {
+  try {
+    done(null, user.id);
+  } catch (err) {
+    done(err);
+  }
+});
 //
-// passport.deserializeUser((id, done) => {
-//   User.findById(id)
-//     .then(user => done(null, user))
-//     .catch(done);
-// });
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => done(null, user))
+    .catch(done);
+});
 //
 //
 // // register google strategy with passport
-// passport.use(new GoogleStrategy(
-//   {
-//     clientID: process.env.GOOGLE_CLIENT_ID,
-//     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//     callbackURL: `http://localhost:${PORT}/api/users/google/redirect`,
-//     passReqToCallback: true
-//   },
-//   function (request, token, refreshToken, profile, done) {
-//     User.findOrCreate({
-//       where: {
-//         google_id: profile.id,
-//         name: profile.displayName,
-//         email: profile.emails[0].value
-//       }
-//     })
-//       .spread((user) => done(null, user))
-//       .catch(done);
-//   }
-// ));
+passport.use(new GoogleStrategy(
+  {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: `http://localhost:${PORT}/api/users/google/redirect`,
+    passReqToCallback: true
+  },
+  function (request, token, refreshToken, profile, done) {
+    console.log('user profile?')
+    return User.findOrCreate({
+      where: {
+        google_id: profile.id,
+        name: profile.displayName,
+        email: profile.emails[0].value
+      }
+    })
+    .spread((user) => done(null, user))
+    .catch(done);
+  }
+));
 
 // // sync database, then start server
-// db.sync(/*{ force: true }*/) // uncomment force true to clear database with each sync
-//   .then(() => {
-app.listen(PORT, () => {
-  console.log("Server listening on Port: ", PORT);
-  var file = "";
-});
-//   })
-//   .catch(err => console.error(err));
+db.sync({ force: true }) // uncomment force true to clear database with each sync
+.then(() => {
+  app.listen(PORT, () => {
+    console.log("Server listening on Port: ", PORT);
+  })
+})
+.catch(err => console.error(err));
 
 //redirect api routes
 app.use("/api", require("./api"));
